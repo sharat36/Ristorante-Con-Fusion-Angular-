c25fcf2ace449c2e30b7354a621a2a06b1731e4d
sharat36 <n.sharat.chandra36@gmail.com>
2020-06-08 21:10:09 +0530

    Basic Services

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Dish } from '../shared/dish';
+
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
@@ -8,7 +9,7 @@ import { Dish } from '../shared/dish';
 export class DishdetailComponent implements OnInit {
 
   @Input()
-  dish = Dish;
+  dish: Dish;
 
   constructor() { }
 
